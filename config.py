ca16287624e45e3f12f8dff6b03990ae5d48286c
Christian Krug <mrminifreddy@gmail.com>
2020-09-15 23:01:17 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+host=""
+user=""
+password=""
+database=""
+apikey=""
+strings={"not_registered": "Player not registered on HyStats", "invalid_key": "Invalid API key",
+            "project_title": "HyStats Private API",
+            "project_description": "This API is intended only for private use within HyStats projects and requires a valid API key.",
+            "project_version": "1.2.0"}
